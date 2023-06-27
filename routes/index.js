9fbd73d3ab4be58b141abb4a54844eebb63e312a
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-27 18:34:27 +0545

    ejs website template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-const express = require("express")
+
+const express = require("express");
 const router = express.Router();
 
-const apiRouter = require("./routes.api")
+const apiRouter = require("./routes.api");
+const uiRouter = require("./routes.ui");
 
-router.use("api/v1",apiRouter);
-router.use("/",apiRouter);
+router.use("/api/v1", apiRouter);
+router.use("/", uiRouter);
 
 module.exports = router;
+
--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -2,8 +2,13 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/",(req,res) =>{
-    res.send({msg: "Hello from UI"});
+    // res.send({msg: "Hello from UI"});
+    res.render("index");
 } );
+
+router.get("/contact",(req,res)=>{
+    res.render("contact");
+})
 
 router.get("/converter/:currency1/:currency2/:value", async (req,res) => {
     const {currency1,currency2,value} = req.params;
