1ae8e1d003ba0c8d7f36616838ab7daffe7709ec
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-27 14:05:11 +0545

    middleware npm

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const ejs = require('ejs');
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
 // })
 const indexRouter = require("./routes");
+app.use(morgan("hello"));
+app.use(cors());
 app.use("/",indexRouter);
+
+//setting up the third party middlewares
+app.use(cors());
+
+// app.use((req,res,next) => {
+// console.log("Request Received at " +Date.now());
+// next();
+// })
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
